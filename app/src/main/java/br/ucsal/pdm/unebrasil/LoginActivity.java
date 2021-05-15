d9adaf1a14e43f3c21d5a47e108b5e718c75356f
ldtsouza <laudilenesouza.7991@aluno.saojudas.br>
2021-05-15 17:20:52 -0300

    150521_ Criamos doador_cadastrar - activity_login e fizemos exclusões. Criamos a classe em java - Login Activity

--- app/src/main/java/br/ucsal/pdm/unebrasil/LoginActivity.java
+++ app/src/main/java/br/ucsal/pdm/unebrasil/LoginActivity.java
@@ -1,17 +1,14 @@
 package br.ucsal.pdm.unebrasil;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity  extends AppCompatActivity {
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_main);
-
-
+        setContentView (R.layout.activity_login);
     }
 }
