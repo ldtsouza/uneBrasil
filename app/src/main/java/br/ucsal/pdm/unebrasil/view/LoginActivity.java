a662374158c0fc3139fa04d1dc5322289046531a
José Antonio Santos <joseantonio.santos@ucsal.edu.br>
2021-05-31 21:39:24 -0300

    310521 - adicionando verificação login

--- app/src/main/java/br/ucsal/pdm/unebrasil/view/LoginActivity.java
+++ app/src/main/java/br/ucsal/pdm/unebrasil/view/LoginActivity.java
@@ -1,15 +1,28 @@
 package br.ucsal.pdm.unebrasil.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import br.ucsal.pdm.unebrasil.R;
+import br.ucsal.pdm.unebrasil.repository.BancoDados;
+import br.ucsal.pdm.unebrasil.repository.BancoDados_Impl;
+import br.ucsal.pdm.unebrasil.repository.dao.BeneficiarioDAO;
+import br.ucsal.pdm.unebrasil.repository.dao.DoacaoDAO;
+import br.ucsal.pdm.unebrasil.repository.dao.DoadorDAO;
 import br.ucsal.pdm.unebrasil.utils.MaskEditUtil;
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,11 +35,17 @@ public class LoginActivity extends AppCompatActivity {
     private boolean tudoOk;
     private static String ERRO_CAMPO = "Campo obrigatório";
 
+    BancoDados db;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_login);
+        db = new BancoDados_Impl();
+        inicializacaoDosCampos();
+    }
 
+    public void inicializacaoDosCampos() {
         edtInputCpf = findViewById(R.id.edtinput_login_cpfId);
         edtInputsenha = findViewById(R.id.edtinput_login_senhaId);
         tilCPF = findViewById(R.id.til_login_cpfId);
@@ -34,6 +53,7 @@ public class LoginActivity extends AppCompatActivity {
 
         edtInputCpf.addTextChangedListener(MaskEditUtil.mask(edtInputCpf, MaskEditUtil.FORMAT_CPF));
     }
+
     public void esqueciMinhaSenha(View view){
         Intent intencao = new Intent(this, EsqueciSenhaActivity.class);
         startActivity(intencao);
@@ -45,37 +65,48 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void cadDoador(View view){
-
         Intent intencao = new Intent(this, DoadorActivity.class);
         startActivity(intencao);
     }
-    public void validarLogin(View view){
 
+    public void validarLogin(View view){
         verificarCampos();
 
         if (tudoOk) {
-            Intent intencao = new Intent(this, MainActivity.class);
-            startActivity(intencao);
+            String cpf = edtInputCpf.getText().toString();
+            String senha = edtInputsenha.getText().toString();
+
+            //db.getDoadorDao().obterTodos();
+
+            if (cpf.equals("999.999.999-99") && senha.equals("123456")) {
+                alert("Login realizado com sucesso!");
+                Intent intencao = new Intent(this, MainActivity.class);
+                startActivity(intencao);
+            } else {
+                alert("CPF ou senha inválido!");
+            }
         }
+    }
+
+    private void alert (String alerta) {
+        Toast.makeText(getApplicationContext(), alerta, Toast.LENGTH_LONG).show();
     }
 
     public boolean verificarCampos() {
         tudoOk = true;
-
         if (edtInputCpf.getText().toString().trim().equals("")) {
             tilCPF.setError(ERRO_CAMPO);
             tudoOk = false;
-        }else {
+        } else {
             tilCPF.setError(null);
         }
 
         if (edtInputsenha.getText().toString().trim().equals("")) {
             tilSenha.setError(ERRO_CAMPO);
             tudoOk = false;
-        }else {
+        } else {
             tilSenha.setError(null);
         }
         return tudoOk;
     }
-
 }
